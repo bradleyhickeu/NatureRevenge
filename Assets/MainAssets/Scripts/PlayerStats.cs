1c83503bad0b1f629c87c3e403a396a3f3a2ec9e
JonathanUmaru <Radishineamigo@hotmail.co.uk>
2016-03-10 17:53:13 +0000

    comments to blood
    f

--- Assets/MainAssets/Scripts/PlayerStats.cs
+++ Assets/MainAssets/Scripts/PlayerStats.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class PlayerStats : MonoBehaviour {
-	public GameObject blood;
+	public GameObject blood; //create public gameobject blood
 	public float healthValue = 15; //create health variable and set it at 100
 	//public Camera Camera;
 	// Use this for initialization
@@ -12,14 +12,14 @@ public class PlayerStats : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		healthValue = healthValue + 0.01f;
+		healthValue = healthValue + 0.01f; //health gradually increases during gameplay
 		Debug.Log (healthValue);
-		if (healthValue > 99) {
-			healthValue = 100;
+		if (healthValue > 99) { //if health is more than 99
+			healthValue = 100; //make health 100 (done this so health doesn't surpass 100
 		}
-		if (healthValue <= 30) 
+		if (healthValue <= 30) //if health is less than 30
 		{
-			//Instantiate(blood, new Vector3 (this.transform.position.x, this.transform.position.y), Quaternion.identity);
+			//Instantiate(blood, new Vector3 (this.transform.position.x, this.transform.position.y), Quaternion.identity); //add blood to scene
 			//blood.transform.localPosition.x;
 		}
 	}
