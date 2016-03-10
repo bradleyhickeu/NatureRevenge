3fbb49e91b39359ced19f059432730392d470e35
JonathanUmaru <Radishineamigo@hotmail.co.uk>
2016-03-10 17:19:04 +0000

    add menu and blood
    yep

---
+++ Assets/MainAssets/Scripts/PlayerStats.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+	public GameObject blood;
+	public float healthValue = 15; //create health variable and set it at 100
+	//public Camera Camera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		healthValue = healthValue + 0.01f;
+		Debug.Log (healthValue);
+		if (healthValue > 99) {
+			healthValue = 100;
+		}
+		if (healthValue <= 30) 
+		{
+			Instantiate(blood, new Vector3 (this.transform.position.x, this.transform.position.y), Quaternion.identity);
+			//blood.transform.localPosition.x;
+		}
+	}
+
+}
