80d503f93ddc36993f6a4635a0622f53503aa9fd
JonathanUmaru <Radishineamigo@hotmail.co.uk>
2016-03-17 15:44:08 +0000

    added blood on player screen

--- Assets/MainAssets/Scripts/PlayerStats.cs
+++ Assets/MainAssets/Scripts/PlayerStats.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class PlayerStats : MonoBehaviour {
 	public GameObject blood; //create public gameobject blood
 	public float healthValue = 15; //create health variable and set it at 100
+	private int distance = 1;
+	public bool bloodOnScreen = false;
+	private GameObject clone;
 	//public Camera Camera;
 	// Use this for initialization
 	void Start () {
@@ -17,10 +20,22 @@ public class PlayerStats : MonoBehaviour {
 		if (healthValue > 99) { //if health is more than 99
 			healthValue = 100; //make health 100 (done this so health doesn't surpass 100
 		}
-		if (healthValue <= 30) //if health is less than 30
+		if (healthValue <= 30  && bloodOnScreen == false) //if health is less than 30
 		{
-			//Instantiate(blood, new Vector3 (this.transform.position.x, this.transform.position.y), Quaternion.identity); //add blood to scene
+			
+			clone = Instantiate(blood, transform.position + transform.forward*distance, transform.rotation)as GameObject; //add blood to scene
+			clone.transform.parent = GameObject.Find("Canvas").transform;
+			bloodOnScreen = true;
+		
 			//blood.transform.localPosition.x;
+
+		}
+		if (healthValue > 30 && bloodOnScreen == true)
+		{
+			Debug.Log("kill");
+			GameObject.Destroy (clone);
+			bloodOnScreen = false;
+
 		}
 	}
 
